2642d90199e7a609963cfcc3ec6a237afd60a39b
thang1911 <braveshiefateubw@gmail.com>
2023-08-08 17:27:56 +0700

    New commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXTAUTH_URL: `https://nextjs-spotify-lac.vercel.app`
+  },
 }
 
 module.exports = nextConfig
